0608427921af9042080b06e37e34ca92d7afcaf6
DESKTOP-ET419MI\den udvalgte <toke_jacobsen@hotmail.com>
2019-07-15 16:44:42 +0200

    start button disabled hvis ikke klar

--- FmDraft/Scripts/scriptInit.js
+++ FmDraft/Scripts/scriptInit.js
@@ -1,5 +1,30 @@
 ﻿window.onload = function () {
 
- //   $("#leagues").css("background-color", "red");
+
+    var t = setInterval(checkIfReady, 1000);
+
+
+    function checkIfReady() {
+        let leagueChosen = $("#leagues").val();
+        let player1 = $("#player1").val() != null && $("#player1").val() != "" ; 
+        let player2 = $("#player2").val() != null && $("#player2").val() != "";
+        let budget = $("#budget").val() != null && $("#budget").val() != "";
+
+        let ready = (leagueChosen.length > 0 && player1 && player2 && budget)
+        if (ready) {
+
+            $(".start").attr("disabled", false);
+          //  $(".start").attr("class", "start-ready");
+
+            //css("background-color", "green");
+        }
+        else {
+            $(".start").attr("disabled", true);
+
+        }
+
+
+    }
 
 }
+
